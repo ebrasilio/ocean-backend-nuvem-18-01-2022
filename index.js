c6fa207125402ecd68a7040c49612236c6ddaeb4
Emilia Brasilio <ebrasilio@gmail.com>
2022-01-18 14:51:20 -0300

    Incluindo banco no mongodb + reativando db

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb://localhost:27017";
+//const url = "mongodb://localhost:27017";
 // const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+const url = "mongodb+srv://admin:admin@cluster0.5clbt.mongodb.net";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-/*
+
     const client = await MongoClient.connect(url);
-
     const db = client.db(dbName);
-
     const collection = db.collection("herois");
-*/
-    const collection = undefined;
-    
-    
+   
     // Aplicação em Express
 
     const app = express();
